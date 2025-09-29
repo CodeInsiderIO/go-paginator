d818bdfc0509069cfa9075a0b33a8bfbb4dc203b
Chi Pham <chipv.bka@gmail.com>
2025-09-29 15:54:00 +0700

    update get page

--- paginator.go
+++ paginator.go
@@ -1,71 +1,81 @@
 package utility
 
-import "math"
-
 type Paginator struct {
 	PerPage     int64 `json:"perPage"`
+	CurrentPage int64 `json:"currentPage"`
+	TotalItems  int64 `json:"totalItems"`
+
+	// Derived
+	TotalPages  int64 `json:"totalPages"`
+	Offset      int64 `json:"offset"`
+	ItemCount   int64 `json:"itemCount"` // số item thực tế ở trang hiện tại
 	HasPrevious bool  `json:"hasPrevious"`
 	HasNext     bool  `json:"hasNext"`
-	Offset      int64 `json:"offset"`
-	CurrentPage int64 `json:"currentPage"`
-	ItemCount   int64 `json:"itemCount"`
-	TotalItems  int64 `json:"totalItems"`
-	TotalPages  int64 `json:"totalPages"`
+	PrevPage    int64 `json:"prevPage"`
+	NextPage    int64 `json:"nextPage"`
 }
 
-func NewPaginator(totalItems int64, currentPage int64, limit int64) *Paginator {
-	paginator := &Paginator{
+func NewPaginator(totalItems, currentPage, limit int64) *Paginator {
+	p := &Paginator{
 		TotalItems:  totalItems,
-		CurrentPage: currentPage,
 		PerPage:     limit,
+		CurrentPage: currentPage,
 	}
-	paginator.Paginate()
-	return paginator
+	p.recompute()
+	return p
 }
 
-func (p *Paginator) Paginate() {
-	p.TotalPages = p.calculateTotalPages()
-	p.CurrentPage = p.getCurrentPage()
-	p.HasNext = p.hasNext()
-	p.HasPrevious = p.hasPrevious()
-	p.ItemCount = p.getItemCount()
-	p.Offset = p.findOffset()
-}
-
-func (p *Paginator) getItemCount() int64 {
-	return p.TotalItems - (p.TotalPages-1)*p.PerPage
-}
-
-func (p *Paginator) hasPrevious() bool {
-	if p.CurrentPage >= 2 {
-		return true
+func (p *Paginator) recompute() {
+	if p.PerPage <= 0 {
+		p.PerPage = 10
 	}
-	return false
-}
 
-func (p *Paginator) hasNext() bool {
-	return p.CurrentPage < p.TotalPages
-}
-
-func (p *Paginator) calculateTotalPages() int64 {
-	return int64(math.Ceil(float64(p.TotalItems) / float64(p.PerPage)))
-}
-
-func (p *Paginator) findOffset() int64 {
-	return (p.getCurrentPage() - 1) * p.PerPage
-}
-
-func (p *Paginator) getCurrentPage() int64 {
-	if p.CurrentPage <= 1 {
-		return 1
+	if p.TotalItems <= 0 {
+		p.TotalItems = 0
+		p.TotalPages = 1
+		p.CurrentPage = 1
+		p.Offset = 0
+		p.ItemCount = 0
+		p.HasPrevious = false
+		p.HasNext = false
+		p.PrevPage = 0
+		p.NextPage = 0
+		return
 	}
-	return p.CurrentPage
+
+	p.TotalPages = (p.TotalItems + p.PerPage - 1) / p.PerPage
+
+	if p.CurrentPage < 1 {
+		p.CurrentPage = 1
+	} else if p.CurrentPage > p.TotalPages {
+		p.CurrentPage = p.TotalPages
+	}
+
+	p.Offset = (p.CurrentPage - 1) * p.PerPage
+
+	if p.CurrentPage < p.TotalPages {
+		p.ItemCount = p.PerPage
+	} else {
+		p.ItemCount = p.TotalItems - (p.PerPage * (p.TotalPages - 1))
+	}
+
+	p.HasPrevious = p.CurrentPage > 1
+	p.HasNext = p.CurrentPage < p.TotalPages
+	if p.HasPrevious {
+		p.PrevPage = p.CurrentPage - 1
+	} else {
+		p.PrevPage = 0
+	}
+	if p.HasNext {
+		p.NextPage = p.CurrentPage + 1
+	} else {
+		p.NextPage = 0
+	}
 }
 
-func (p *Paginator) getNextPage() int64 {
-	return p.getCurrentPage()
-}
-
-func (p *Paginator) findLastPage() int64 {
-	return p.calculateTotalPages()
+func (p *Paginator) Set(totalItems, currentPage, limit int64) {
+	p.TotalItems = totalItems
+	p.CurrentPage = currentPage
+	p.PerPage = limit
+	p.recompute()
 }
